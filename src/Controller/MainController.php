61ef73d662b069a23fcd7dc7ae87c672f4f46f0d
petrero <petrero@gmail.com>
2025-01-12 19:11:11 +0200

    6.3 Using the QueryBuilder  - One bummer is that DQL isn't that pretty! Luckily, Doctrine has a "query builder". This thing is awesome: instead of writing the DQL string manually, we build it with an object. Back in our homepage() method, replace $em->createQuery() with $em->createQueryBuilder(). Off it, chain ->select('s'), then ->from(Starship::class, 's') hitting tab add the use statement from App\Entity. Bonus! We can use Starship::class instead of the string.
     Finally, before ->getResult(), call ->getQuery().
    
     Back in the app, refresh the homepage... still works!
    
     We still need to refactor one thing. Click on one of the ships... oh no!
    
      Starship not found.
    
     Ahh, our StarshipController::show() action is still using the old StarshipRepository with the hardcoded data. We need to fix that!

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Starship;
 use App\Model\StarshipRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,7 +14,10 @@ class MainController extends AbstractController {
 	public function homepage(
 		EntityManagerInterface $em,
 	): Response {
-		$ships = $em->createQuery('select s from App\Entity\Starship s')
+		$ships = $em->createQueryBuilder()
+			->select('s')
+			->from(Starship::class, 's')
+			->getQuery()
 			->getResult();
 		$myShip = $ships[array_rand($ships)];
 
