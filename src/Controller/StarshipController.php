8b712a47a9b70f277c2a05af379f1c6a06d12cde
petrero <petrero@gmail.com>
2025-01-12 19:39:41 +0200

    7.3 Let's also use this in StarshipController::show(). Replace EntityManagerInterface with StarshipRepository $repository
     Every repository also comes pre-built with a find() method! And because this is the Starship repository, we don't need to pass the entity class - just the $id
    
     Jump back to the app, refresh, and click a starship. Still works, perfect!

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Starship;
+use App\Repository\StarshipRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,8 +11,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipController extends AbstractController {
 	#[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
-	public function show(int $id, EntityManagerInterface $em): Response {
-		$ship = $em->find(Starship::class, $id);
+	public function show(int $id, StarshipRepository $repository): Response {
+		$ship = $repository->find($id);
 		if (!$ship) {
 			throw $this->createNotFoundException('Starship not found');
 		}
