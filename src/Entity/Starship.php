91c070425ec9782265470518e8cacad214fef364
petrero <petrero@gmail.com>
2025-01-13 20:44:44 +0200

    10.1 Starship Upgrade: Adding Slug and Timestamp Fields - symfony console make:entity Starship
     New requirements have come down from the admirals at Starfleet HQ. Instead of seeing the id in the URL, like /starship/1, they want to see a human-readable name, like /starship/enterprise. This is called a "slug". To accomplish this, we need to add a new field to our Starship entity.
    
     Adding Fields to an Existing Entity
     - We could add this by hand, super-easily: add the property, getter, setter, and the #[ORM\Column] attribute. Or we can cheat! Run:
    
      symfony console make:entity Starship
     Instead of creating a new entity, this time we'll add fields to an existing entity. Add slug, type string, length 255. "Should it be nullable?" - no, but choose yes for now. Let's add 2 more handy fields: updatedAt, datetime_immutable, nullable? yes, temporarily, and createdAt, datetime_immutable, nullable? yes.
    
     Hit Enter to exit the command. Before creating the migration, go check the Starship entity: src/Entity/Starship.php

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -28,6 +28,15 @@ class Starship {
 	#[ORM\Column]
 	private ?DateTimeImmutable $arrivedAt = null;
 
+	#[ORM\Column(length: 255, nullable: true)]
+	private ?string $slug = null;
+
+	#[ORM\Column(nullable: true)]
+	private ?\DateTimeImmutable $updatedAt = null;
+
+	#[ORM\Column(nullable: true)]
+	private ?\DateTimeImmutable $createdAt = null;
+
 	public function getId(): ?int {
 		return $this->id;
 	}
@@ -92,5 +101,35 @@ class Starship {
 			StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
 			StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
 		};
+	}
+
+	public function getSlug(): ?string {
+		return $this->slug;
+	}
+
+	public function setSlug(?string $slug): static {
+		$this->slug = $slug;
+
+		return $this;
+	}
+
+	public function getUpdatedAt(): ?\DateTimeImmutable {
+		return $this->updatedAt;
+	}
+
+	public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static {
+		$this->updatedAt = $updatedAt;
+
+		return $this;
+	}
+
+	public function getCreatedAt(): ?\DateTimeImmutable {
+		return $this->createdAt;
+	}
+
+	public function setCreatedAt(?\DateTimeImmutable $createdAt): static {
+		$this->createdAt = $createdAt;
+
+		return $this;
 	}
 }
