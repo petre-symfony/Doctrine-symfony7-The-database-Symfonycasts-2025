8c90260ca79a83a1b0c84ad66d7eb67855850bd0
petrero <petrero@gmail.com>
2025-01-17 11:29:45 +0200

    15.1 Quantum Refactor: Rich Entities
     Take a look at our Starship entity. It's a bunch of properties and getters and setters. Kind of boring, right? It doesn't have to be! Since entities are standard PHP classes, we can add meaningful, explicit methods that describe our business logic, like goToWarp(7) or enterOrbitAround(). These are called rich entity methods.
    
     Let's try this out and explore the benefits.
    
     Reduce Duplication
     - Our Starship check-in logic currently lives in the ShipCheckInCommand::execute() method. After we fetch the ship, we update its arrivedAt and status. What if, in the future, we add a check-in controller. We'd have to duplicate this logic there. And if the logic for "checking in" changes - like we need to update another field, we'd have to remember to change it in multiple places. That is super not sci-fi.
    
     Adding a Starship::checkIn() Method
     - The better way is to move, or encapsulate, this check-in logic into a method on the entity. Open src/Entity/Starship.php and scroll to the bottom. Create a new: public function checkIn(). Have it accept an optional ?\DateTimeImmutable  = null and return static, which is a fancy way of saying "return the current object"
    
     return $this
    
     Above, add the check-in logic: $this->arrivedAt = $arrivedAt, and if it wasn't passed, ?? new \DateTimeImmutable('now'). Next, $this->status = StarshipStatusEnum::WAITING

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -137,4 +137,11 @@ class Starship {
 
 		return $this;
 	}
+
+	public function checkIn(?\DateTimeImmutable $arrivedAt = null): static {
+		$this->arrivedAt = $arrivedAt ?? new \DateTimeImmutable('now');
+		$this->status = StarshipStatusEnum::WAITING;
+		
+		return $this;
+	}
 }
